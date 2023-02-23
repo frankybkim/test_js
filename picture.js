7b74a57f0959fe0cb1dc119f77ee32653afc92fc
Yoon Kim <frank.yb.kim@gmail.com>
2023-02-23 06:54:26 -0800

    Create picture.js

---
+++ picture.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Display Image Example</title>
+  </head>
+  <body>
+    <img id="myImage" src="" alt="My Image">
+    <button onclick="displayImage()">Display Image</button>
+
+    <script>
+      function displayImage() {
+        var img = document.getElementById("myImage");
+        img.src = "https://static.boredpanda.com/blog/wp-content/uploads/2014/01/animal-children-photography-elena-shumilova-2.jpg";
+      }
+    </script>
+  </body>
+</html>
